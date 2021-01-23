cd82a1b7e6e391e4daaff2c6665dc59388ea76b3
SharkArslan <arslan.maqsood@sharkmea.com>
2021-01-24 01:39:23 +0500

    countdown vuejs Project

---
+++ countdown/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+   <div> Hello World</div>
+   <Counter
+   :year="2021"
+   :month="4"
+   :date="25"
+   :hour="23"
+   :minute="59"
+   :second="59"
+   :millisecond="31"/>
+  </div>
+  
+</template>
+
+<script>
+import Counter from '@/components/Counter.vue';
+// @ is an alias to /src
+export default {
+  components: {
+    Counter 
+  }
+};
+</script>
